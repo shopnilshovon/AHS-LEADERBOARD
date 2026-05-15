75e20e21e148224be754be7ad23f8acc13029bfa
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-15 15:16:38 +0600

    Add calculateEarning function for commission calculation

---
+++ js/commission-system.js
@@ -0,0 +1,23 @@
+function calculateEarning(name,earning){
+
+const upper=name.toUpperCase();
+
+if(
+upper.startsWith("SM_") ||
+upper.startsWith("TT") ||
+upper.startsWith("TT_") ||
+upper.startsWith("AH_")
+){
+return earning*0.80;
+}
+
+if(
+upper.startsWith("AHS") ||
+upper.startsWith("AHS_")
+){
+return earning*0.90;
+}
+
+return earning;
+
+}
