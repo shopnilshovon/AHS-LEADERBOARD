d7608a17ccfd248155c1680656e849210c5e072a
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-15 19:32:30 +0600

    Update alltime-data.js

--- js/alltime-data.js
+++ js/alltime-data.js
@@ -11,14 +11,31 @@ earning:45.600
 name:"AHS_Dip",
 country:"Bangladesh",
 sms:2100,
-earning:39.200
+earning:35.280
+},
+
+{
+name:"TTsohanur",
+country:"Bangladesh",
+sms:1800,
+earning:28.700
 },
 
 {
 name:"AHS_Rocky",
 country:"Bangladesh",
-sms:1900,
-earning:33.400
+sms:1500,
+earning:25.400
+},
+
+{
+name:"AHS_jerry",
+country:"Bangladesh",
+sms:1200,
+earning:20.100
 }
 
 ];
+
+const alltimeUpdate =
+"15 May 2026 • 7:27 PM";
