0b57b152f0ed8e203f0745bfac0481e3ae3453be
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-15 21:51:49 +0600

    Update leaderboard-data.js

--- js/leaderboard-data.js
+++ js/leaderboard-data.js
@@ -15,31 +15,38 @@ earning:3.125
 },
 
 {
-name:"AHS_jerry",
+name:"AHS_Rocky",
 country:"Bangladesh",
-sms:100,
-earning:1.900
+sms:162,
+earning:2.922
 },
 
 {
-name:"AHS_Rocky",
+name:"AHS_jerry",
 country:"Bangladesh",
-sms:98,
-earning:1.779
+sms:115,
+earning:2.185
 },
 
 {
 name:"tltraihan",
 country:"Bangladesh",
-sms:89,
-earning:1.268
+sms:94,
+earning:1.338
 },
 
 {
 name:"SM_SHAKIB",
 country:"Bangladesh",
-sms:81,
-earning:1.685
+sms:84,
+earning:1.742
+},
+
+{
+name:"AH_Sujon",
+country:"Bangladesh",
+sms:69,
+earning:1.211
 },
 
 {
@@ -50,17 +57,10 @@ earning:1.283
 },
 
 {
-name:"AH_Sujon",
-country:"Bangladesh",
-sms:53,
-earning:0.907
-},
-
-{
 name:"AHS_Shanto",
 country:"Bangladesh",
-sms:45,
-earning:0.797
+sms:48,
+earning:0.839
 },
 
 {
@@ -87,4 +87,4 @@ earning:0.014
 ];
 
 const dailyUpdate =
-"15 May 2026 • 8:55 PM";
+"15 May 2026 • 9:49 PM";
