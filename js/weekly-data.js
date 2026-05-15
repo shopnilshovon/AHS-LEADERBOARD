9a6ebeb2dc35dc0f90b34b45644c2dfc7884012e
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-15 19:32:04 +0600

    Create weekly-data.js

---
+++ js/weekly-data.js
@@ -0,0 +1,41 @@
+const weeklyUsers = [
+
+{
+name:"AHS_Dip",
+country:"Bangladesh",
+sms:818,
+earning:12.514
+},
+
+{
+name:"AHS_Rocky",
+country:"Bangladesh",
+sms:530,
+earning:8.473
+},
+
+{
+name:"SHOVON",
+country:"Bangladesh",
+sms:518,
+earning:9.490
+},
+
+{
+name:"TTsohanur",
+country:"Bangladesh",
+sms:490,
+earning:7.820
+},
+
+{
+name:"AHS_jerry",
+country:"Bangladesh",
+sms:430,
+earning:6.980
+}
+
+];
+
+const weeklyUpdate =
+"15 May 2026 • 7:27 PM";
