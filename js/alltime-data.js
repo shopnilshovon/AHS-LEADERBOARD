40c46e94da10a3085849d6c6405f01d63f99cc0d
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-15 19:05:10 +0600

    Create alltime-data.js

---
+++ js/alltime-data.js
@@ -0,0 +1,24 @@
+const alltimeUsers = [
+
+{
+name:"SHOVON",
+country:"Bangladesh",
+sms:2500,
+earning:45.600
+},
+
+{
+name:"AHS_Dip",
+country:"Bangladesh",
+sms:2100,
+earning:39.200
+},
+
+{
+name:"AHS_Rocky",
+country:"Bangladesh",
+sms:1900,
+earning:33.400
+}
+
+];
