79e4deca7ff565f200ca9ed164f903cedd070d2b
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-16 06:46:32 +0600

    Update weekly-data.js

--- js/weekly-data.js
+++ js/weekly-data.js
@@ -3,71 +3,71 @@ const weeklyUsers = [
 {
 name:"AHS_Dip",
 country:"Bangladesh",
-sms:818,
-earning:13.905
+sms:853,
+earning:14.554
 },
 
 {
 name:"AHS_Rocky",
 country:"Bangladesh",
-sms:670,
-earning:11.925
-},
-
-{
-name:"tltraihan",
-country:"Bangladesh",
-sms:522,
-earning:10.467
+sms:757,
+earning:13.530
 },
 
 {
 name:"SHOVON",
 country:"Bangladesh",
-sms:522,
-earning:9.571
+sms:737,
+earning:10.723
 },
 
 {
 name:"tlttoufiq909",
 country:"Bangladesh",
-sms:495,
-earning:8.221
+sms:539,
+earning:8.949
+},
+
+{
+name:"tltraihan",
+country:"Bangladesh",
+sms:528,
+earning:10.551
 },
 
 {
 name:"AHS_Obito",
 country:"Bangladesh",
-sms:407,
-earning:6.486
+sms:409,
+earning:6.520
 },
 
 {
 name:"SM_SHAKIB",
 country:"Bangladesh",
-sms:369,
-earning:6.862
+sms:389,
+earning:7.302
 },
 
 {
 name:"AHS_Shanto",
 country:"Bangladesh",
-sms:318,
-earning:5.952
+sms:332,
+earning:6.190
 },
 
 {
 name:"TTsohanur",
 country:"Bangladesh",
-sms:224,
-earning:4.219
+sms:235,
+earning:4.428
 },
 
 {
 name:"AHS_jerry",
 country:"Bangladesh",
-sms:139,
-earning:2.645
+sms:160,
+earning:3.040
 },
 
 {
@@ -87,8 +87,8 @@ earning:1.871
 {
 name:"AH_Sujon",
 country:"Bangladesh",
-sms:69,
-earning:1.211
+sms:70,
+earning:1.230
 },
 
 {
@@ -103,6 +103,13 @@ name:"AHS_Nihon",
 country:"Bangladesh",
 sms:45,
 earning:0.945
+},
+
+{
+name:"AH_Limon",
+country:"Bangladesh",
+sms:7,
+earning:0.133
 },
 
 {
@@ -128,5 +135,11 @@ earning:0.014
 
 ];
 
+/*
+Unnamed SMS Added To SHOVON
+154 + 36 = 190 SMS
+0.717 earning added
+*/
+
 const weeklyUpdate =
-"15 May 2026 • 11:00 PM";
+"16 May 2026 • 6:44 AM";
