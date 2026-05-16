29e3e8a286f89d0e53baa05586ce8a60f69a49b9
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-16 18:19:00 +0600

    Create payment-info.js

---
+++ js/payment-info.js
@@ -0,0 +1,104 @@
+const paymentInfo = {
+
+"AHS_Nihon":{
+bkash:"",
+binance:"513727479"
+},
+
+"AHS_Shanto":{
+bkash:"01758532664",
+binance:""
+},
+
+"AHS_Rocky":{
+bkash:"01737118509",
+binance:""
+},
+
+"AHS_Obito":{
+bkash:"",
+binance:"1044837693"
+},
+
+"AHS_Dip":{
+bkash:"01765729573",
+binance:""
+},
+
+"AHS_jerry":{
+bkash:"01865654694",
+binance:"817186064"
+},
+
+"TTsohanur":{
+bkash:"",
+binance:"780073755"
+},
+
+"AH_Sujon":{
+bkash:"",
+binance:"791572814"
+},
+
+"AH_Najmul":{
+bkash:"01792593284",
+binance:"1097792720"
+},
+
+"AH_Limon":{
+bkash:"",
+binance:"422252977"
+},
+
+"SHOVON":{
+bkash:"",
+binance:"794889158"
+},
+
+/* NO PAYMENT INFO */
+
+"AH_Rana":{
+message:"Please add your payment details."
+},
+
+"SM_SHAKIB":{
+message:"Please add your payment details."
+},
+
+"tltrakibul":{
+message:"Please add your payment details."
+},
+
+"tltnahid":{
+message:"Please add your payment details."
+},
+
+"tltrahat":{
+message:"Please add your payment details."
+},
+
+"tlttoufiq9092":{
+message:"Please add your payment details."
+},
+
+"tltraihan":{
+message:"Please add your payment details."
+},
+
+"tlttusher":{
+message:"Please add your payment details."
+},
+
+"tltrafat":{
+message:"Please add your payment details."
+},
+
+"tlttoufiq9091":{
+message:"Please add your payment details."
+},
+
+"tlttoufiq909":{
+message:"Please add your payment details."
+}
+
+};
