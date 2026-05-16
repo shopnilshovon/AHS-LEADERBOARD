726153004bb46a5f2c372b83e1e2603392d335a8
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-16 11:18:12 +0600

    Update leaderboard-data.js

--- js/leaderboard-data.js
+++ js/leaderboard-data.js
@@ -1,34 +1,67 @@
 const users = [
 
 {
-name:"SHOVON",
+name:"SM_SHAKIB",
 country:"Bangladesh",
-sms:22,
-earning:0.454
+sms:167,
+earning:3.249
 },
 
 {
-name:"SM_SHAKIB",
+name:"AHS_Rocky",
 country:"Bangladesh",
-sms:20,
-earning:0.440
+sms:90,
+earning:1.496
+},
+
+{
+name:"SHOVON",
+country:"Bangladesh",
+sms:38,
+earning:0.741
 },
 
 {
 name:"tlttoufiq909",
 country:"Bangladesh",
-sms:4,
-earning:0.072
+sms:30,
+earning:0.490
 },
 
 {
 name:"AHS_Shanto",
 country:"Bangladesh",
-sms:2,
-earning:0.040
+sms:26,
+earning:0.448
+},
+
+{
+name:"tltraihan",
+country:"Bangladesh",
+sms:19,
+earning:0.266
+},
+
+{
+name:"TTsohanur",
+country:"Bangladesh",
+sms:12,
+earning:0.183
+},
+
+{
+name:"AH_Sujon",
+country:"Bangladesh",
+sms:9,
+earning:0.171
 }
 
 ];
 
+/*
+Unnamed SMS Added To SHOVON
+2 SMS + $0.023
+*/
+
 const dailyUpdate =
-"16 May 2026 • 6:40 AM";
+"16 May 2026 • 11:19 AM";
