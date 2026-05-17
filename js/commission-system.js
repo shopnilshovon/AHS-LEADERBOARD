c966d037ccda8da225b016ffc36746ff237664f8
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-17 18:24:57 +0600

    Update commission-system.js

--- js/commission-system.js
+++ js/commission-system.js
@@ -6,14 +6,14 @@ if(
 upper.startsWith("SM_") ||
 upper.startsWith("TT") ||
 upper.startsWith("TT_") ||
+upper.startsWith("TLT") ||
+upper.startsWith("TLT_") ||
 upper.startsWith("AH_")
 ){
 return earning*0.80;
 }
 
 if(
-upper.startsWith("TLT") ||
-upper.startsWith("TLT_") ||
 upper.startsWith("AHS") ||
 upper.startsWith("AHS_")
 ){
