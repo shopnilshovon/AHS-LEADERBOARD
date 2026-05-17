8fd90b95755f9f93640ef7c7732d5e3d13a64314
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-17 21:52:35 +0600

    Update leaderboard-data.js

--- js/leaderboard-data.js
+++ js/leaderboard-data.js
@@ -3,8 +3,8 @@ const users = [
 {
 name:"tltraihan",
 country:"Bangladesh",
-sms:301,
-earning:4.493
+sms:308,
+earning:4.591
 },
 
 {
@@ -17,36 +17,36 @@ earning:4.578
 {
 name:"Toufiq",
 country:"Bangladesh",
-sms:222,
-earning:3.244
-},
-
-{
-name:"AHS_jerry",
-country:"Bangladesh",
-sms:120,
-earning:1.939
+sms:234,
+earning:3.489
 },
 
 {
 name:"AHS_Dip",
 country:"Bangladesh",
-sms:117,
-earning:2.051
+sms:124,
+earning:2.226
+},
+
+{
+name:"AHS_jerry",
+country:"Bangladesh",
+sms:123,
+earning:1.981
 },
 
 {
 name:"AHS_Obito",
 country:"Bangladesh",
-sms:113,
-earning:1.908
+sms:118,
+earning:2.033
 },
 
 {
 name:"AHS_Rocky",
 country:"Bangladesh",
-sms:85,
-earning:1.506
+sms:91,
+earning:1.656
 },
 
 {
@@ -59,15 +59,15 @@ earning:1.596
 {
 name:"AH_Limon",
 country:"Bangladesh",
-sms:52,
-earning:0.988
+sms:65,
+earning:1.235
 },
 
 {
 name:"AHS_Shanto",
 country:"Bangladesh",
-sms:46,
-earning:0.827
+sms:55,
+earning:1.052
 },
 
 {
@@ -85,17 +85,17 @@ earning:0.405
 },
 
 {
-name:"AH_Najmul",
+name:"TLT_Shahin",
 country:"Bangladesh",
-sms:16,
+sms:20,
 earning:0.280
 },
 
 {
-name:"TLT_Shahin",
+name:"AH_Najmul",
 country:"Bangladesh",
-sms:11,
-earning:0.154
+sms:16,
+earning:0.280
 },
 
 {
@@ -108,4 +108,4 @@ earning:0.018
 ];
 
 const dailyUpdate =
-"17 May 2026 • 9:34 PM";
+"17 May 2026 • 9:50 PM";
