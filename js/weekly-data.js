558ebea0c81d03eb173467fcf37c3b760580a489
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-26 08:23:11 +0600

    Update weekly-data.js

--- js/weekly-data.js
+++ js/weekly-data.js
@@ -1,101 +1,101 @@
 const weeklyUsers = [
 
 {
-name:"tltraihan",
+name:"TTsohanur",
 country:"Bangladesh",
-sms:2101,
-earning:36.095
+sms:3590,
+earning:66.669
 },
 
 {
-name:"TTsohanur",
+name:"tltraihan",
 country:"Bangladesh",
-sms:1826,
-earning:32.129
+sms:3035,
+earning:51.199
 },
 
 {
 name:"Toufiq",
 country:"Bangladesh",
-sms:1668,
-earning:28.617
-},
-
-{
-name:"SHOVON",
-country:"Bangladesh",
-sms:1336,
-earning:19.322
-},
-
-{
-name:"AHS_jerry",
-country:"Bangladesh",
-sms:1214,
-earning:21.694
+sms:2356,
+earning:42.975
 },
 
 {
 name:"tltnahid",
 country:"Bangladesh",
-sms:1134,
-earning:18.470
+sms:1694,
+earning:27.207
 },
 
 {
-name:"AHS_Rocky",
+name:"AHS_jerry",
 country:"Bangladesh",
-sms:972,
-earning:17.173
-},
-
-{
-name:"AHS_Dip",
-country:"Bangladesh",
-sms:830,
-earning:14.338
+sms:1542,
+earning:27.426
 },
 
 {
 name:"SM_SHAKIB",
 country:"Bangladesh",
-sms:607,
-earning:13.183
+sms:1151,
+earning:24.516
 },
 
 {
 name:"AHS_Shanto",
 country:"Bangladesh",
-sms:653,
-earning:11.886
+sms:1085,
+earning:20.130
 },
 
 {
-name:"tltrafat",
+name:"AHS_Rocky",
 country:"Bangladesh",
-sms:528,
-earning:9.474
+sms:1084,
+earning:19.621
+},
+
+{
+name:"SHOVON",
+country:"Bangladesh",
+sms:1058,
+earning:19.875
+},
+
+{
+name:"AHS_Dip",
+country:"Bangladesh",
+sms:1010,
+earning:17.669
 },
 
 {
 name:"AHS_Obito",
 country:"Bangladesh",
-sms:549,
-earning:9.274
+sms:650,
+earning:11.395
 },
 
 {
 name:"AH_Limon",
 country:"Bangladesh",
-sms:402,
-earning:8.468
+sms:553,
+earning:11.639
+},
+
+{
+name:"tltrafat",
+country:"Bangladesh",
+sms:529,
+earning:9.497
 },
 
 {
 name:"AHS_Nihon",
 country:"Bangladesh",
-sms:211,
-earning:3.643
+sms:318,
+earning:5.775
 },
 
 {
@@ -108,15 +108,15 @@ earning:2.883
 {
 name:"AH_Sujon",
 country:"Bangladesh",
-sms:25,
-earning:0.469
+sms:40,
+earning:0.708
 },
 
 {
-name:"Toufiq9091",
+name:"tltmustafiz",
 country:"Bangladesh",
-sms:4,
-earning:0.076
+sms:10,
+earning:0.230
 },
 
 {
@@ -124,9 +124,16 @@ name:"AHS_Jehan",
 country:"Bangladesh",
 sms:6,
 earning:0.150
+},
+
+{
+name:"Toufiq9091",
+country:"Bangladesh",
+sms:4,
+earning:0.076
 }
 
 ];
 
 const weeklyUpdate =
-"24 May 2026 • 1:46 AM";
+"19-25 May 2026 • 1:48 PM";
